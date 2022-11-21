439f6966c6694994b6658c1c4ee649f00be05ffd
tami <xorud7979@gmail.com>
2022-11-21 10:31:18 +0900

    first commit

---
+++ test.ts
@@ -0,0 +1,32 @@
+type Words = {
+    [key: string]: string
+  }
+  
+  class Dict {
+    private words: Words
+    constructor() {
+      this.words = {}
+    }
+    add(word: Word) { // class Word를 타입처럼 사용이 가능
+      if (this.words[word.term] === undefined) {
+         this.words[word.term] = word.def;
+      }
+    }
+    def(term:string){ // 조회 메소드
+      return this.words[term]
+    }
+  }
+  
+  class Word {
+    constructor(
+      public term: string,
+      public def: string
+    ) {}
+  }
+  
+  const kimchi = new Word("kimchi", "한국의 음식");
+  
+  const dict = new Dict()
+  
+  dict.add(kimchi);
+  dict.def("kimchi")
